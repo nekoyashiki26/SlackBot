9f213d7fba8a3b9885fc5259c53b978525bdeb9f
Yoshinori Yamaguchi <b1518292@planet.kanazawa-it.ac.jp>
2017-06-09 21:43:55 +0900

    テストファイルの削除

--- bot.rb
+++ bot.rb
@@ -5,7 +5,7 @@ require 'eventmachine'
 require 'faye/websocket'
 
 response = HTTP.post("https://slack.com/api/rtm.start", params: {
-    token: ENV['SLACK_API_TOKEN']
+    token: ENV['SLACK_API_TOKEN1']
 })
 
 rc = JSON.parse(response.body)
@@ -14,43 +14,49 @@ url = rc['url']
 
 EM.run do
   # Web Socketインスタンスの立ち上げ
-  ws = Faye::WebSocket::Client.new(url)
+  webSocket = Faye::WebSocket::Client.new(url)
 
   #  接続が確立した時の処理
-  ws.on :open do
+  webSocket.on :open do
     p [:open]
   end
 
   # RTM APIから情報を受け取った時の処理
   # 接続が切断した時の処理
-  ws.on :close do
+  webSocket.on :close do
     p [:close, event.code]
-    ws = nil
+    webSocket = nil
     EM.stop
   end
 
-  ws.on :message do |event|
+  webSocket.on :message do |event|
     data = JSON.parse(event.data)
     p [:message, data]
 
     if data['text'] == 'こんにちは' then
-      ws.send({
+      webSocket.send({
         type: 'message',
         text: "こんにちは <@#{data['user']}> ",
         channel: data['channel']
       }.to_json)
     elsif data['text'] == 'こんばんは' then
-      ws.send({
+      webSocket.send({
         type: 'message',
         text: "こんばんは <@#{data['user']}> ",
         channel: data['channel']
         }.to_json)
     elsif data['text'] == 'おはよう' then
-      ws.send({
+      webSocket.send({
         type: 'message',
         text: "こんにちは <@#{data['user']}> ",
         channel: data['channel']
       }.to_json)
+    elsif data['text'] == '' then
+      webSocket.send({
+          type: 'message',
+          text: 'message',
+          channel: data['channel']
+        }.to_json)
     end
   end
 end
--- test.rb
+++
@@ -1,8 +0,0 @@
-# test.rb
-require 'http'
-require 'json'
-
-response = HTTP.post("https://slack.com/api/auth.test", params: {
-    token: ENV['SLACK_API_TOKEN']
-  })
-puts JSON.pretty_generate(JSON.parse(response.body))
